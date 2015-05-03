a03110217ad59e479b6d6e099c72737c18f0d4d2
Brian Gaeddert <bgaeddert@gmail.com>
2015-05-02 23:16:45 -0600

    Removed auth.basic

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,18 +15,18 @@
 // HomeController is constructed.
 // If you are not logged in you get the login page.
 // If you are you get the Angular page (resources/views/index.php)
-Route::get('/', 'HomeController@index');
+Route::get( '/', 'HomeController@index' );
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::controllers( [
+    'auth'     => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+] );
 
 // Api routes that can only be called from Auth users
-Route::group(array('prefix'=>'/api'),function(){
-    Route::get('users',['middleware' => 'auth.basic', 'uses' => 'UserController@index']);
-    Route::post('users/test',['middleware' => 'auth.basic', 'uses' => 'UserController@postTest']);
-});
+Route::group( [ 'prefix' => '/api', 'middleware' => 'auth' ], function (){
+    Route::get( 'users', 'UserController@index' );
+    Route::post( 'users/test', 'UserController@postTest' );
+} );
 
 // Catch all route send you to the homeController. (index page)
-Route::any('{url?}', 'HomeController@index')->where(['url' => '[-a-z0-9/]+']);
+Route::any( '{url?}', 'HomeController@index' )->where( [ 'url' => '[-a-z0-9/]+' ] );
