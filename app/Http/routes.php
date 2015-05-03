96e66cf7128ee298e17c167938a93cdb182bf5fe
Brian Gaeddert <bgaeddert@gmail.com>
2015-05-02 19:58:16 -0600

    Route comments

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,21 +11,22 @@
 |
 */
 
+// Auth is handled by the middleware when the
+// HomeController is constructed.
+// If you are not logged in you get the login page.
+// If you are you get the Angular page (resources/views/index.php)
 Route::get('/', 'HomeController@index');
-
-//Route::get('home', 'HomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
 
+// Api routes that can only be called from Auth users
 Route::group(array('prefix'=>'/api'),function(){
-    //Route::get('users','UserController@index', ['middleware' => 'auth']);
     Route::get('users',['middleware' => 'auth.basic', 'uses' => 'UserController@index']);
     Route::post('users/test',['middleware' => 'auth.basic', 'uses' => 'UserController@postTest']);
 });
 
-//Route::any( '{catchall}', 'HomeController@index')->where('catchall', '(.*)');
-
+// Catch all route send you to the homeController. (index page)
 Route::any('{url?}', 'HomeController@index')->where(['url' => '[-a-z0-9/]+']);
